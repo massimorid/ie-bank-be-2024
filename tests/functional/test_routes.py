545154cfa8b1cb8c8eae17201ee2d85e70989514
massimorid <mridella.ieu2022@student.ie.edu>
2024-10-12 12:03:30 +0200

    small c for country

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -26,7 +26,7 @@ def test_create_account(testing_client):
     WHEN the '/accounts' page is posted to (POST)
     THEN check the response is valid
     """
-    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€', 'Country': 'Italy'})
+    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€', 'country': 'Italy'})
     assert response.status_code == 200
 
 
